bc66743edbe5cfe626e68f3083de3642a8ee0a1a
kazhe94 <evgeniisherbakov@mail.ru>
2020-03-28 19:17:21 +0300

    адаптив хэдера под мобильные устройства, добавлена кнопка бургер, реализован стоп скролл

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $( document ).ready(function() {
-    $('.menu-btn').on('click', function(){
-        $('.nav-menu ul').toggleClass('.nav-menu-mobile');
+    $('.menu-btn').on('click', function(e){
+        e.preventDefault;
+        $('.nav-menu').toggleClass('active');
+        $('.menu-btn').toggleClass('menu-btn_active')
+        $('body').toggleClass('stop-scroll');
     });
 });
 
