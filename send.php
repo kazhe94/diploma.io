951eb853633e13deebca3fc72fe6211f521cb9f8
kazhe94 <evgeniisherbakov@mail.ru>
2020-03-31 01:07:56 +0300

    добавлен скрипт отправки формы на почту

---
+++ send.php
@@ -0,0 +1,63 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $msg = "ok";
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";                                          
+    $mail->SMTPAuth   = true;
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера GMAIL
+    $mail->Username   = 'web.shcherbakov@inbox.ru'; // Логин на почте
+    $mail->Password   = '9Hc-ycg-ycd-9ER'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('web.shcherbakov@inbox.ru'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('evgeniisherbakov@mail.ru');  
+
+    // Прикрипление файлов к письму
+if (!empty($_FILES['myfile']['name'][0])) {
+    for ($ct = 0; $ct < count($_FILES['myfile']['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['myfile']['name'][$ct]));
+        $filename = $_FILES['myfile']['name'][$ct];
+        if (move_uploaded_file($_FILES['myfile']['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+        } else {
+            $msg .= 'Неудалось прикрепить файл ' . $uploadfile;
+        }
+    }   
+}
+
+        // -----------------------
+        // Само письмо
+        // -----------------------
+        $mail->isHTML(true);
+    
+        $mail->Subject = 'Запрос на создание сайта';
+        $mail->Body    = "<b>Имя:</b> $name <br>
+        <b>Почта:</b> $email<br><br>
+        <b>Сообщение:</b><br>$phone";
+
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {
+    echo "$msg";
+} else {
+echo "Сообщение не было отправлено. Неверно указаны настройки вашей почты";
+}
+
+} catch (Exception $e) {
+    echo "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
